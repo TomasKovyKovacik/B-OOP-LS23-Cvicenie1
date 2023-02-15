c1f9aa1b5783850292627a248c094ec1e4918772
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2023-02-15 16:09:14 +0100

    primitivne datove typy

--- src/main/java/sk/stuba/fei/oop/App.java
+++ src/main/java/sk/stuba/fei/oop/App.java
@@ -2,6 +2,21 @@ package sk.stuba.fei.oop;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int cislo = 5;
+        long celeVacsieCislo = 5L;
+        float desCislo = 5.5f;
+        double dlhsieDesCislo = 5.55555555;
+        char znak = 'a';
+        boolean bool = false;
+        String string = "toto je text";
+
+        int podiel = 2/3; //0
+        System.out.println(podiel);
+
+        int zvysok = 2%3; //2
+        System.out.println(zvysok);
+
+        float podielFloat = 2/3.0f;
+        System.out.println(podielFloat);
     }
 }
