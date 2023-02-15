130f4af0ea5777314ab3e01b36b60840550a83d8
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2023-02-15 16:14:57 +0100

    for

--- src/main/java/sk/stuba/fei/oop/App.java
+++ src/main/java/sk/stuba/fei/oop/App.java
@@ -2,16 +2,17 @@ package sk.stuba.fei.oop;
 
 public class App {
     public static void main(String[] args) {
-        int cislo = 2;
-        switch (cislo) {
-            case 1:
-                System.out.println("nieco");
-                break;
-            case 2:
-                System.out.println("nieco 2");
-                break;
-            default:
-                System.out.println("ani jedno z toho");
+        for (int cislo = 1; cislo < 3; cislo++) {
+            switch (cislo) {
+                case 1:
+                    System.out.println("nieco");
+                    break;
+                case 2:
+                    System.out.println("nieco 2");
+                    break;
+                default:
+                    System.out.println("ani jedno z toho");
+            }
         }
     }
 }
