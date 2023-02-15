ce7d6d68e1786c00666663d6f046374288186f1c
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2023-02-15 16:19:22 +0100

    praca s polom a funkcia

--- src/main/java/sk/stuba/fei/oop/App.java
+++ src/main/java/sk/stuba/fei/oop/App.java
@@ -2,19 +2,18 @@ package sk.stuba.fei.oop;
 
 public class App {
     public static void main(String[] args) {
-        int cislo = 1;
-        do {
-            switch (cislo) {
-                case 1:
-                    System.out.println("nieco");
-                    break;
-                case 2:
-                    System.out.println("nieco 2");
-                    break;
-                default:
-                    System.out.println("ani jedno z toho");
-            }
-            cislo++;
-        } while (cislo < 1);
+        int[] pole = new int[10];
+
+        for (int i=0; i <pole.length; i++) {
+            pole[i] = i;
+        }
+
+        for (int i=0; i <pole.length; i++) {
+            vypis(pole[i]);
+        }
+    }
+
+    public static void vypis(int a) {
+        System.out.println("Cislo v poli je " + a);
     }
 }
